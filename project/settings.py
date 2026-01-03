9c3b0c9a7e2079765784c5714bb83e22bf8aa6e0
Peterkhater <peter.2006khater@gmail.com>
2026-01-03 14:55:24 +0200

    ready to deploy v1

--- project/settings.py
+++ project/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-fr2w-kl7q)=*b-_tso=j2s1^48)=yyg-@n50mu5(n@*ulpk9ek'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['JosephAbboud.pythonanywhere.com', 'www.JosephAbboud.pythonanywhere.com', 'localhost','127.0.0.1']
 
 INSTALLED_APPS = [
     "jazzmin",
@@ -111,3 +111,35 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
+
+secure_proxy_ssl_header = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Joseph Abboud Admin",
+    "site_header": "Joseph Abboud",
+    "welcome_sign": "Welcome to the Joseph Abboud Admin",
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-dark bg-primary",
+    "accent": "accent-primary",
+    "navbar": "navbar-dark bg-primary",
+    "footer": "footer-dark bg-primary",
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_nav_compact": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,    
+}
+
